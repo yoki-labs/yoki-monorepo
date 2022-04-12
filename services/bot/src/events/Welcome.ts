e9e00f57d742156004d729070a6786932e198a38
zaida04 <nico.03727+github@gmail.com>
2022-04-12 19:11:54 -0400

    fix: assign client#userId to the proper id

--- services/bot/src/events/Welcome.ts
+++ services/bot/src/events/Welcome.ts
@@ -3,6 +3,6 @@ import type { WSWelcomePayload } from "@guildedjs/guilded-api-typings";
 import type Client from "../Client";
 
 export default (data: WSWelcomePayload["d"]["user"], client: Client) => {
-    client.userId = data.botId;
+    client.userId = data.id;
     client.ownerId = data.createdBy;
 };
