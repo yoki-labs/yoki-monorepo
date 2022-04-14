90dd3f64e3c55a03e767f220aba892d532863da0
zaida04 <nico.03727+github@gmail.com>
2022-04-14 18:11:17 -0400

    ci: revert to droplet

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+cd ./docker/compose
 if [ $# -eq 0 ]; then
     echo "Running local up -d --build"
-    docker-compose up -d --build 
-    docker-compose logs --follow bot 
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build 
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml logs --follow bot 
 elif [ $1 == "db" ]; then
-	docker-compose up -d --build db
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build db
 elif [ $1 == "down" ]; then
     echo "Running local down"
-    docker-compose down ${@:2}
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml down ${@:2}
 elif [ $1 == "logs" ]; then
 	echo "Attaching to bot logs"
-    docker-compose logs -f bot
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml logs -f bot
 else
     echo "Invalid args."
 fi
