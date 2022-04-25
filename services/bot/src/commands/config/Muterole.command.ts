119e750c623759e35f80c5b45bb0868bd1b58113
Nico <nico.03727+github@gmail.com>
2022-04-25 13:45:53 -0400

    fix: mute role Id

--- services/bot/src/commands/config/Muterole.command.ts
+++ services/bot/src/commands/config/Muterole.command.ts
@@ -17,7 +17,7 @@ const Muterole: Command = {
 
         if (!newRole) {
             const muteRole = commandCtx.server.muteRoleId;
-            return ctx.messageUtil.send(message.channelId, muteRole ? `The mute role is set to: \`${muteRole.muteRoleId}\`` : `There is no mute role channel set.`);
+            return ctx.messageUtil.send(message.channelId, muteRole ? `The mute role is set to: \`${muteRole}\`` : `There is no mute role channel set.`);
         }
 
         await ctx.prisma.server.updateMany({ data: { muteRoleId: newRole }, where: { serverId: message.serverId! } });
