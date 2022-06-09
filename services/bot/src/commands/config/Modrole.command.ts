41a1654c84dbd1e44d53736b72e6aae7a64f3aa2
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2022-06-09 21:01:02 +0300

    feat: muterole and modrole: role mentions, ability to remove them

--- services/bot/src/commands/config/Modrole.command.ts
+++ services/bot/src/commands/config/Modrole.command.ts
@@ -10,42 +10,57 @@ const Modrole: Command = {
     name: "config-modrole",
     description: "Add a mod role.",
     subCommand: true,
-    usage: "[newRole] [mod/admin]",
-    examples: ["12345678", "12345678 admin"],
+    usage: "[role ID] [mod/admin/remove]",
+    examples: ["12345678", "12345678 admin", "12345678 remove"],
     subName: "modrole",
     category: Category.Settings,
     requiredRole: RoleType.ADMIN,
     args: [
-        { name: "newRole", type: "number", optional: true },
+        { name: "role", type: "number", optional: true },
         { name: "staffLevel", type: "string", optional: true },
     ],
     execute: async (message, args, ctx) => {
-        const newRole = (args.newRole as number) ?? null;
+        const modroleId = (args.role as number) ?? null;
         const levelArg = (args.staffLevel as string)?.toUpperCase();
-        const staffLevel = RoleType[allowedTypes.includes(levelArg) ? levelArg : "MOD"] ?? RoleType.MOD;
 
-        if (!newRole) {
-            const modRoles = await ctx.prisma.role.findMany({ where: { serverId: message.serverId, type: staffLevel } });
-            return modRoles.length
-                ? ctx.messageUtil.replyWithInfo(
-                      message,
-                      `Staff roles`,
-                      `Here are the staff roles for this server:\n- ${modRoles.map((modRole) => `${inlineCode(modRole.roleId)} (${modRole.type})`).join("\n- ")}`
-                  )
-                : ctx.messageUtil.replyWithNullState(message, `No staff roles`, `There are no staff roles set for this server yet.`);
+        if (levelArg === "REMOVE") {
+            if (!(await ctx.prisma.role.findFirst({ where: { serverId: message.serverId, roleId: modroleId } })))
+                return ctx.messageUtil.replyWithAlert(message, `Not a staff role`, `The given role is not a mod/admin role or does not exist.`);
+
+            await ctx.prisma.role.deleteMany({ where: { roleId: modroleId, serverId: message.serverId } });
+
+            return ctx.messageUtil.replyWithSuccess(message, `Removed staff role`, `<@${modroleId}> is no longer a staff role.`, undefined, { isSilent: true });
+        } else {
+            const staffLevel = RoleType[allowedTypes.includes(levelArg) ? levelArg : "MOD"] ?? RoleType.MOD;
+
+            if (!modroleId) {
+                const modRoles = await ctx.prisma.role.findMany({ where: { serverId: message.serverId } });
+                return modRoles.length
+                    ? ctx.messageUtil.replyWithInfo(
+                          message,
+                          `Staff roles`,
+                          `Here are the staff roles for this server:\n- ${modRoles.map((modRole) => `<@${modRole.roleId}> (${inlineCode(modRole.type)})`).join("\n- ")}`,
+                          undefined,
+                          {
+                              isSilent: true,
+                          }
+                      )
+                    : ctx.messageUtil.replyWithNullState(message, `No staff roles`, `There are no staff roles set for this server yet.`);
+            }
+            const existing = await ctx.prisma.role.findMany({ where: { serverId: message.serverId, roleId: modroleId, type: staffLevel } });
+            if (existing.find((x) => x.roleId === modroleId))
+                return ctx.messageUtil.replyWithAlert(message, `Already a staff role`, `This role has already been set as ${staffLevel}.`);
+
+            const newModRole = await ctx.prisma.role.create({
+                data: {
+                    roleId: modroleId,
+                    type: staffLevel,
+                    serverId: message.serverId!,
+                },
+            });
+
+            return ctx.messageUtil.replyWithSuccess(message, `Staff role added`, `Successfully set the ${staffLevel} role to ${inlineCode(newModRole.roleId)}`);
         }
-        const existing = await ctx.prisma.role.findMany({ where: { serverId: message.serverId, roleId: newRole, type: staffLevel } });
-        if (existing.find((x) => x.roleId === newRole)) return ctx.messageUtil.replyWithAlert(message, `Already a staff role`, `This role has already been set as ${staffLevel}.`);
-
-        const newModRole = await ctx.prisma.role.create({
-            data: {
-                roleId: newRole,
-                type: staffLevel,
-                serverId: message.serverId!,
-            },
-        });
-
-        return ctx.messageUtil.replyWithSuccess(message, `Staff role added`, `Successfully set the ${staffLevel} role to ${inlineCode(newModRole.roleId)}`);
     },
 };
 
--- services/bot/src/commands/config/Muterole.command.ts
+++ services/bot/src/commands/config/Muterole.command.ts
@@ -1,4 +1,3 @@
-import { inlineCode } from "../../formatters";
 import { RoleType } from "../../typings";
 import { Category } from "../Category";
 import type { Command } from "../Command";
@@ -6,25 +5,39 @@ import type { Command } from "../Command";
 const Muterole: Command = {
     name: "config-muterole",
     description: "Set or view the mute role for this server.",
-    usage: "[newRole]",
+    usage: "[role ID/remove]",
     examples: ["12345678", ""],
     category: Category.Settings,
     subCommand: true,
     subName: "muterole",
     requiredRole: RoleType.ADMIN,
-    args: [{ name: "newRole", optional: true, type: "string" }],
+    args: [{ name: "role", optional: true, type: "string" }],
     execute: async (message, args, ctx, commandCtx) => {
-        const newRole = args.newRole ? Number(args.newRole as string) : null;
+        const role = args.role as string | undefined;
 
-        if (!newRole) {
+        if (!role) {
+            // Just display if it's unspecified
             const muteRole = commandCtx.server.muteRoleId;
+
             return muteRole
-                ? ctx.messageUtil.replyWithInfo(message, `Mute role`, `The mute role is set to role ${inlineCode(muteRole)}.`)
+                ? ctx.messageUtil.replyWithInfo(message, `Mute role`, `The mute role is set to role <@${muteRole}>.`, undefined, { isSilent: true })
                 : ctx.messageUtil.replyWithNullState(message, `No mute role`, `There is no mute role set.`);
+        } else if (role.toUpperCase() === "REMOVE") {
+            // The ability to delete mute roles
+            await ctx.prisma.server.updateMany({ data: { muteRoleId: null }, where: { serverId: message.serverId } });
+
+            return ctx.messageUtil.replyWithSuccess(message, `Mute role removed`, `<@${commandCtx.server.muteRoleId}> is no longer a mute role.`, undefined, { isSilent: true });
         }
 
-        await ctx.prisma.server.updateMany({ data: { muteRoleId: newRole }, where: { serverId: message.serverId! } });
-        return ctx.messageUtil.replyWithSuccess(message, `Mute role set`, `Successfully set ${inlineCode(newRole)} as the mute role`);
+        const roleId = role ? Number(role as string) : null;
+
+        // This could be merged back with `if (!role)`, but I am too lazy to handle it well with
+        // `REMOVE` + it is better to tell someone they are wrong than just make them confused
+        if (Number.isNaN(roleId))
+            return ctx.messageUtil.replyWithAlert(message, `Provide role ID`, `Provide the ID of the role you want to set as a mute role or type \`remove\` to remove it.`);
+
+        await ctx.prisma.server.updateMany({ data: { muteRoleId: roleId }, where: { serverId: message.serverId! } });
+        return ctx.messageUtil.replyWithSuccess(message, `Mute role set`, `Successfully set <@${roleId}> as the mute role`, undefined, { isSilent: true });
     },
 };
 
