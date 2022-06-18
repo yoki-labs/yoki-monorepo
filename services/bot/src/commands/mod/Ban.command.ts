b5e5414df76c774ce59ee72c59a576a58f21a384
zaida04 <nico.03727+github@gmail.com>
2022-06-18 14:06:54 -0400

    feat: kick and ban

---
+++ services/bot/src/commands/mod/Ban.command.ts
@@ -0,0 +1,66 @@
+import { stripIndents } from "common-tags";
+import { inlineCode } from "../../formatters";
+import { CachedMember, RoleType } from "../../typings";
+import { Category } from "../Category";
+import type { Command } from "../Command";
+
+const Ban: Command = {
+    name: "ban",
+    description: "Ban a user",
+    usage: "<targetId> [...reason]",
+    examples: ["R40Mp0Wd", "R40Mp0Wd Talking too much about Town of Salem"],
+    requiredRole: RoleType.MOD,
+    category: Category.Moderation,
+    args: [
+        {
+            name: "target",
+            type: "member",
+        },
+        {
+            name: "reason",
+            type: "rest",
+            optional: true,
+            max: 500,
+        },
+    ],
+    execute: async (message, args, ctx) => {
+        const target = args.target as CachedMember;
+        const reason = args.reason as string | null;
+
+        try {
+            await ctx.rest.router.banMember(message.serverId!, target.user.id);
+        } catch (e) {
+            return ctx.messageUtil.replyWithError(
+                message,
+                stripIndents`
+					There was an issue banning this user. This is most likely due to misconfigured permissions for your server.
+					${inlineCode((e as Error).message)}
+				`,
+                undefined,
+                { isPrivate: true }
+            );
+        }
+
+        await ctx.dbUtil.addActionFromMessage(message, {
+            infractionPoints: 10,
+            reason,
+            targetId: target.user.id,
+            type: "BAN",
+            expiresAt: null,
+        });
+
+        await ctx.messageUtil.sendSuccessBlock(
+            message.channelId,
+            `User banned`,
+            `<@${message.createdBy}>, you have successfully banned ${target.user.name} (${inlineCode(target.user.id)}).`,
+            undefined,
+            {
+                isPrivate: true,
+            }
+        );
+
+        return ctx.rest.router.deleteChannelMessage(message.channelId, message.id);
+    },
+};
+
+export default Ban;
---
+++ services/bot/src/commands/mod/Kick.command.ts
@@ -0,0 +1,66 @@
+import { stripIndents } from "common-tags";
+import { inlineCode } from "../../formatters";
+import { CachedMember, RoleType } from "../../typings";
+import { Category } from "../Category";
+import type { Command } from "../Command";
+
+const Kick: Command = {
+    name: "kick",
+    description: "Kick a user",
+    usage: "<targetId> [...reason]",
+    examples: ["R40Mp0Wd", "R40Mp0Wd Talking too much about Town of Salem"],
+    requiredRole: RoleType.MOD,
+    category: Category.Moderation,
+    args: [
+        {
+            name: "target",
+            type: "member",
+        },
+        {
+            name: "reason",
+            type: "rest",
+            optional: true,
+            max: 500,
+        },
+    ],
+    execute: async (message, args, ctx) => {
+        const target = args.target as CachedMember;
+        const reason = args.reason as string | null;
+
+        try {
+            await ctx.rest.router.kickMember(message.serverId!, target.user.id);
+        } catch (e) {
+            return ctx.messageUtil.replyWithError(
+                message,
+                stripIndents`
+					There was an issue kicking this user. This is most likely due to misconfigured permissions for your server.
+					${inlineCode((e as Error).message)}
+				`,
+                undefined,
+                { isPrivate: true }
+            );
+        }
+
+        await ctx.dbUtil.addActionFromMessage(message, {
+            infractionPoints: 10,
+            reason,
+            targetId: target.user.id,
+            type: "KICK",
+            expiresAt: null,
+        });
+
+        await ctx.messageUtil.sendSuccessBlock(
+            message.channelId,
+            `User kicked`,
+            `<@${message.createdBy}>, you have successfully kicked ${target.user.name} (${inlineCode(target.user.id)}).`,
+            undefined,
+            {
+                isPrivate: true,
+            }
+        );
+
+        return ctx.rest.router.deleteChannelMessage(message.channelId, message.id);
+    },
+};
+
+export default Kick;
