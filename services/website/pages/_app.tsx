c85813bd68cc62a2c9005009461ae5df2237c7bf
zaida04 <nico.03727+github@gmail.com>
2022-07-12 17:36:36 -0400

    feat: ogimage

--- services/website/pages/_app.tsx
+++ services/website/pages/_app.tsx
@@ -11,7 +11,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>Yoki</title>
-                <meta name="description" content="Guilded's first moderation bot." />
+
+                <title>Yoki</title>
+                <meta name="description" content="Guilded's first moderation bot. Complete with automoderation, nsfw image filtering, and modmail." />
+
+                <meta property="og:url" content="https://yoki.gg/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Yoki" />
+                <meta property="og:description" content="Guilded's first moderation bot. Complete with automoderation, nsfw image filtering, and modmail." />
+                <meta property="og:image" content="https://yoki.gg/face.png" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="yoki.gg" />
+                <meta property="twitter:url" content="https://yoki.gg/" />
+                <meta name="twitter:title" content="Yoki" />
+                <meta name="twitter:description" content="Guilded's first moderation bot. Complete with automoderation, nsfw image filtering, and modmail." />
+                <meta name="twitter:image" content="https://yoki.gg/face.png" />
+
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar />
