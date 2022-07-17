ebb0a9ec02b04ec189c32ace2c17648049b6e584
Nico <nico.03727+github@gmail.com>
2022-07-16 23:45:43 -0400

    chore: add starboard invite

--- services/website/next.config.js
+++ services/website/next.config.js
@@ -27,6 +27,11 @@ const nextConfig = {
             destination: "https://www.guilded.gg/Yoki/groups/2dXLMBPd/channels/c8532cb8-fe5f-4aa9-abf8-267142c199d8/forums",
             permanent: false,
         },
+        {
+            source: "/invite/starboard",
+            destination: "https://www.guilded.gg/b/e1ddaa4f-bdb3-45e4-b98d-ae49d6ed75b8",
+            permanent: true
+        }
     ],
     eslint: {
         // Warning: This allows production builds to successfully complete even if
