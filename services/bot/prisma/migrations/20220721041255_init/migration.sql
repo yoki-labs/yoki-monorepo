a759d87355692d2216e08d15d6d1b9ebbd93be13
Nico <nico.03727+github@gmail.com>
2022-07-21 00:18:01 -0400

    feat: set account age filter

---
+++ services/bot/prisma/migrations/20220721041255_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Server" ADD COLUMN     "antiRaidAgeFilter" INTEGER;
---
+++ services/bot/src/commands/antiraid/Age.command.ts
@@ -0,0 +1,29 @@
+import { ResponseType } from "@prisma/client";
+import ms from "ms";
+import { RoleType } from "../../typings";
+import { Category } from "../Category";
+import type { Command } from "../Command";
+
+const Age: Command = {
+    name: "antiraid-age",
+    description: "Set or view the minimum account age requirement for users to not be caught in the antiraid",
+    usage: "[duration]",
+    examples: ["2d"],
+    category: Category.Moderation,
+    subCommand: true,
+    subName: "age",
+    requiredRole: RoleType.ADMIN,
+    args: [{ name: "duration", type: "string", optional: true }],
+    execute: async (message, args, ctx, commandCtx) => {
+        if (!args.duration) {
+            return ctx.messageUtil.replyWithInfo(message, "Test", "placeholder")
+        }
+
+        const duration = ms(args.duration as string);
+        if (!duration || duration <= 600000 || duration >= 1209600000) return ctx.messageUtil.replyWithAlert(message, `Invalid Duration`, `Your duration must be between 10m and 2w.`);
+        await ctx.prisma.server.update({ where: { id: commandCtx.server.id }, data: { antiRaidAgeFilter: duration, antiRaidResponse: ResponseType.CAPTCHA } })
+        return ctx.messageUtil.replyWithSuccess(message, "Successfully set age filter", `Accounts younger than ${duration / 60 / 1000} minutes will be caught in the filter. By default, the bot will present them with a captcha to solve, but you can configure this using the \`antiraid response\` command.`)
+    },
+};
+
+export default Age;
