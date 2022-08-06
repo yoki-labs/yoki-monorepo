77049a129fd21d55901c30e1f353987145894e2f
Dylan Warren <dylanwarren19@gmail.com>
2022-08-05 23:32:05 -0500

    feat: update/upgrade commands page (#27)
    * feat: Add styled-components
    
    * feat: Accordion component
    
    * feat: New commands component and layout
    
    * chore: move `@types/styled-components` into devDependencies before dylan notices
    
    * feat: Add styled-components
    
    * feat: Accordion component
    
    * feat: New commands component and layout
    
    * chore: move `@types/styled-components` into devDependencies before dylan notices
    
    Co-authored-by: Nico <nico.03727+github@gmail.com>

--- services/website/pages/commands.tsx
+++ services/website/pages/commands.tsx
@@ -1,9 +1,11 @@
-import { Accordion, AccordionBody, AccordionHeader } from "@material-tailwind/react";
 import type { NextPage } from "next";
 import { useState } from "react";
 
 import commands from "../commands.json";
+import Accordion from "../components/accordion/Accordion";
+import { AccordionBodyContent, AccordionHeaderText } from "../components/accordion/styles";
 import { Command } from "../lib/Command";
+import { CommandNavigation, CommandTop, UpArrow } from "../styles/components/commands";
 
 interface GroupedCommands {
     [x: string]: Command[];
@@ -23,55 +25,78 @@ export const getStaticProps = () => {
 };
 
 const Commands: NextPage<{ commandByCategory: GroupedCommands }> = ({ commandByCategory }) => {
-    const [open, setOpen] = useState<string | null>(null);
-
-    const handleOpen = (value: string) => {
-        setOpen(open === value ? null : value);
-    };
+    const categories = Object.keys(commandByCategory);
+    const [activeItem, setActiveItem] = useState<string>(categories[0]);
 
     return (
-        <div className="pt-8 space-y-5 px-14">
-            {Object.keys(commandByCategory).map((category) => (
-                <div className="space-y-5" key={category}>
-                    <h1 className="text-3xl text-white font-bold pb-2">{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
-                    {commandByCategory[category ?? "general"].map((command, index) => {
-                        return (
-                            <Accordion
-                                key={`${category}-${index}`}
-                                open={open === `${category}-${index}`}
-                                className="bg-custom-gray text-white rounded-md w-full md:w-full py-1 px-10"
-                            >
-                                <AccordionHeader onClick={() => handleOpen(`${category}-${index}`)}>
-                                    <h1 className="text-xl text-white font-semibold">{command.name}</h1>
-                                </AccordionHeader>
-                                <AccordionBody>
-                                    <p className="text-white">Description: {command.description}</p>
-                                    <p className="text-white">Examples: {command.examples?.join(", ") ?? `?${command.name}`}</p>
-                                    <p className="text-white">Usage: {`?${command.name}${command.usage ? ` ${command.usage}` : ""}`}</p>
-                                    <p className="text-white">Aliases: {command.aliases?.join(", ") ?? "N/A"}</p>
-                                    <p className="text-white">Subcommands: {command.subCommands?.map((x) => x.name).join(", ") ?? "None"}</p>
-                                    <p className="text-white">Required Role: {command.requiredRole ?? "None"}</p>
-                                    {command.args?.length ? (
-                                        <div className="pt-4">
-                                            <p className="text-xl text-white">Args:</p>
-                                            <ol className="list-decimal text-white pl-8 pt-1">
-                                                {command.args.map((x) => (
-                                                    <li key={`${category}-${index}-${x.name}`}>
-                                                        {x.name} - {x.type}
-                                                        {x.optional ? ` (OPTIONAL)` : ""}
-                                                    </li>
-                                                ))}
-                                            </ol>
+        <div className="flex flex-col lg:flex-row justify-center md:px-20 mx-auto scroll-smooth">
+            <div className="md:pt-4 mx-6 md:mx-0">
+                <CommandNavigation className="pt-4">
+                    <h1 className="text-3xl pb-4">Categories</h1>
+                    <CommandTop
+                        href="#"
+                        className="px-2 flex justify-center items-center gap-2"
+                        onClick={() => {
+                            setActiveItem("");
+                            window.scrollTo({ top: 0, behavior: "smooth" });
+                        }}
+                    >
+                        <UpArrow></UpArrow>
+                        Top
+                    </CommandTop>
+                    {categories.map((c) => (
+                        <a key={c} className={c === activeItem ? "active" : ""} onClick={() => setActiveItem(c)} href={`#${c}`}>
+                            {c.charAt(0).toUpperCase() + c.slice(1)}
+                        </a>
+                    ))}
+                </CommandNavigation>
+            </div>
+            <div className="pt-8 flex flex-col gap-10 space-y-5 px-6 md:px-14">
+                {categories.map((category) => (
+                    <div className="space-y-5" key={category}>
+                        <h1 className="text-3xl text-white text-custom-guilded pb-2" id={category}>
+                            {category.charAt(0).toUpperCase() + category.slice(1)}
+                        </h1>
+                        {commandByCategory[category ?? "general"].map((command, index) => (
+                            <Accordion key={`${category}-${index}`}>
+                                <AccordionHeaderText>{command.name}</AccordionHeaderText>
+                                <AccordionBodyContent>
+                                    <i>{command.description}</i>
+                                    <div className="pl-4">
+                                        <div className="flex flex-col py-2">
+                                            <span>USAGE:</span>
+                                            <code className="mt-2">{`?${command.name}${command.usage ? ` ${command.usage}` : ""}`}</code>
                                         </div>
-                                    ) : (
-                                        <></>
+                                        <div className="flex flex-col py-2">
+                                            <span>EXAMPLES:</span>
+                                            <div className="mt-2 flex flex-col gap-1">
+                                                {command.examples?.map((c, i) => <code key={`example-${command.name}-${i}`}>{c}</code>) ?? <code>{`?${command.name}`}</code>}
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col py-2">
+                                            <span>ALIASES:</span> {command.aliases?.map((a, i) => <code key={`alias-${command.name}-${i}`}>{a}</code>) ?? "None!"}
+                                        </div>
+                                        <div className="flex flex-col py-2">
+                                            <span>SUB COMMANDS:</span>
+                                            <div className="mt-2 flex flex-col gap-1">
+                                                {command.subCommands?.map((c, i) => <code key={`sub-${c.name}-${i}`}>{`?${c.name.split("-").join(" ")} ${c.usage}`}</code>) ??
+                                                    "None!"}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    {command.requiredRole && (
+                                        <div className="flex items-center">
+                                            <span className="text-sm">ROLES REQUIRED</span>
+                                            <span className="ml-2 px-2 role">{command.requiredRole}</span>
+                                        </div>
                                     )}
-                                </AccordionBody>
+                                </AccordionBodyContent>
                             </Accordion>
-                        );
-                    })}
-                </div>
-            ))}
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
---
+++ services/website/styles/components/commands.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const CommandNavigation = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    min-width: 250px;
+
+    position: sticky;
+    top: 1rem;
+
+    height: fit-content;
+    color: white;
+
+    padding: 20px;
+    margin-top: 1rem;
+    border-radius: 5px;
+    background-color: var(--grey);
+
+    a {
+        margin: 0;
+        padding: 5px 10px;
+        border-radius: 5px;
+        color: var(--guilded-yellow);
+        transition: all 0.2s;
+    }
+
+    a:hover {
+        background-color: var(--grey-fade);
+    }
+
+    a.active {
+        background-color: var(--guilded-yellow);
+        color: black;
+    }
+
+    & a {
+        text-decoration: none;
+    }
+`;
+
+export const UpArrow = styled.i`
+    border: solid var(--light-grey);
+    border-width: 0 3px 3px 0;
+    padding: 3px;
+    display: inline-block;
+    transform: rotate(-135deg);
+    -webkit-transform: rotate(-135deg);
+`;
+
+export const CommandTop = styled.a`
+    padding: 5px 10px;
+    border-radius: 5px;
+
+    &:hover {
+        cursor: pointer;
+        background-color: var(--grey-fade);
+    }
+`;
