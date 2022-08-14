68b58e29051c584c6d60a389dd9ec9c8e9871104
Dylan Warren <dylanwarren19@gmail.com>
2022-08-14 18:23:29 -0500

    update: Small changes to preview

--- services/website/components/previews/StatusPreview.tsx
+++ services/website/components/previews/StatusPreview.tsx
@@ -4,12 +4,15 @@ import { StatusWrapper } from "./styles";
 export default function StatusPreview(props: { statusTitle: string; statusDescription: string }) {
     return (
         <StatusWrapper>
-            <div className="hammer">
-                <Hammer></Hammer>
-            </div>
-            <div className="pl-4">
-                <p className="text-lg font-bold text-white">{props.statusTitle}</p>
-                <p className="text-sm">{props.statusDescription}</p>
+            <div className="bg"></div>
+            <div className="flex z-10">
+                <div className="hammer">
+                    <Hammer></Hammer>
+                </div>
+                <div className="pl-4">
+                    <p className="text-lg font-bold text-white">{props.statusTitle}</p>
+                    <p className="text-sm">{props.statusDescription}</p>
+                </div>
             </div>
         </StatusWrapper>
     );
