72bec016ad3fbd472a7607be94aa5890834ad01c
zaida04 <nico.03727+github@gmail.com>
2022-08-23 14:22:22 -0400

    chore: fix kick case

--- services/bot/src/events/TeamMemberRemoved.ts
+++ services/bot/src/events/TeamMemberRemoved.ts
@@ -12,7 +12,7 @@ export default async (packet: WSTeamMemberRemovedPayload, ctx: Context) => {
     const { userId, serverId, isBan, isKick } = packet.d;
 
     if (isBan) void ctx.amp.logEvent({ event_type: "MEMBER_BAN", user_id: userId, event_properties: { serverId } });
-    else if (isBan) void ctx.amp.logEvent({ event_type: "MEMBER_KICK", user_id: userId, event_properties: { serverId } });
+    else if (isKick) void ctx.amp.logEvent({ event_type: "MEMBER_KICK", user_id: userId, event_properties: { serverId } });
 
     // check if there's a log channel channel for member leaves
     const memberLeaveLogChannel = await ctx.dbUtil.getLogChannel(serverId!, LogChannelType.member_leaves);
