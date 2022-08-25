036df80d218b2c0148f79287117a27302acb7777
zaida04 <nico.03727+github@gmail.com>
2022-08-24 21:06:19 -0400

    chore: grey, gray, what's the difference

--- services/website/components/previews/FeaturePreview.tsx
+++ services/website/components/previews/FeaturePreview.tsx
@@ -3,7 +3,7 @@ export default function FeaturePreview(props: { header: string; shortHeader: str
         <div className="py-10 md:py-20 md:grid gap-20 grid-cols-2 place-items-center">
             <div className="text-white">
                 <h1 className="text-4xl font-bold pb-2">{props.header}</h1>
-                <h2 className="text-xl text-grey-500 font-bold capitalize pb-2">{props.shortHeader}</h2>
+                <h2 className="text-xl text-gray-500 font-bold capitalize pb-2">{props.shortHeader}</h2>
                 <p>{props.description}</p>
             </div>
             <img src={props.src} className={`rounded-lg pt-4 md:pt-0  ${props.position === "left" ? "-order-1" : ""}`} />
