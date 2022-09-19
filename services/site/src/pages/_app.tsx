b8b6aaa48400a6f66c4b093222c815143f7d04e9
zaida04 <nico.03727+github@gmail.com>
2022-09-18 23:49:09 -0400

    feat: remerge services

---
+++ services/site/src/pages/_app.tsx
@@ -0,0 +1,45 @@
+import "../styles/globals.css";
+import "../styles/styles.css";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import Footer from "../components/landing/footer/Footer";
+import Navbar from "../components/landing/navbar/navbar";
+const ogDescription = "Meet Yoki, your moderation companion. Guilded's first moderation bot.";
+const ogUrl = "https://yoki.gg/";
+const ogFace = "https://yoki.gg/face.png";
+const ogTitle = "Yoki";
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>{ogTitle}</title>
+                <meta name="description" content={ogDescription} />
+
+                <meta property="og:url" content={ogUrl} />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content={ogTitle} />
+                <meta property="og:description" content={ogDescription} />
+                <meta property="og:image" content={ogFace} />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="yoki.gg" />
+                <meta property="twitter:url" content={ogUrl} />
+                <meta name="twitter:title" content={ogTitle} />
+                <meta name="twitter:description" content={ogDescription} />
+                <meta name="twitter:image" content={ogFace} />
+
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+            <Component {...pageProps} />
+            <div className="bg-[#15171d] px-12 md:px-20">
+                <Footer />
+            </div>
+        </>
+    );
+}
+
+export default MyApp;
