e4fb29a5e3b43d3c6b8a6f9ddc15c4a914f7c8d5
Nico <nico.03727+github@gmail.com>
2022-10-14 11:54:52 -0400

    fix: make range non-inclusive

--- services/bot/src/commands/antiraid/Age.command.ts
+++ services/bot/src/commands/antiraid/Age.command.ts
@@ -21,7 +21,7 @@ const Age: Command = {
         }
 
         const duration = ms(args.duration as string);
-        if (!duration || duration <= 600000 || duration >= 1209600000)
+        if (!duration || duration < 600000 || duration > 1209600000)
             return ctx.messageUtil.replyWithError(message, `Invalid Duration`, `Your duration must be between 10m and 2w.`);
         void ctx.amp.logEvent({ event_type: "ANTIRAID_AGE_SET", user_id: message.createdBy, event_properties: { serverId: message.serverId, age: duration } });
         await ctx.prisma.server.update({ where: { id: commandCtx.server.id }, data: { antiRaidAgeFilter: duration, antiRaidResponse: ResponseType.CAPTCHA } });
