dbcb85fe4bf86a4c506b75a2702aba6ed605cd78
zaida04 <nico.03727+github@gmail.com>
2022-11-19 03:28:17 -0500

    chore: fix migration

--- yoki/bot/prisma/migrations/20221119060714_init/migration.sql
+++ yoki/bot/prisma/migrations/20221119060714_init/migration.sql
@@ -7,7 +7,6 @@
   - A unique constraint covering the columns `[serverId,targetServerId]` on the table `InviteFilter` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[serverId,channelId,type]` on the table `LogChannel` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[messageId]` on the table `Message` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[serverId,preset]` on the table `Preset` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[serverId,channelId,messageId,emoteId,actionType]` on the table `ReactionAction` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[serverId,roleId,type]` on the table `Role` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[serverId,domain]` on the table `UrlFilter` will be added. If there are existing duplicate values, this will fail.
@@ -30,9 +29,6 @@ CREATE UNIQUE INDEX "LogChannel_serverId_channelId_type_key" ON "LogChannel"("se
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Message_messageId_key" ON "Message"("messageId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Preset_serverId_preset_key" ON "Preset"("serverId", "preset");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ReactionAction_serverId_channelId_messageId_emoteId_actionT_key" ON "ReactionAction"("serverId", "channelId", "messageId", "emoteId", "actionType");
