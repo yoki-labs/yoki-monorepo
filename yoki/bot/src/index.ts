b3e0b17e6b45a934844de08388cd738ad008a299
zaida04 <nico.03727+github@gmail.com>
2022-11-19 16:58:33 -0500

    fix: ignore errors on private message sending for captchas

--- yoki/bot/src/index.ts
+++ yoki/bot/src/index.ts
@@ -30,7 +30,6 @@ client.ws.emitter.on("gatewayEvent", async (event, data) => {
 		void client.errorHandler.send("Error creating/fetching server for gateway event.", [errorEmbed(err, { server: serverId, event })])
 	);
     if (!serverFromDb || serverFromDb?.blacklisted) return void 0;
-
     return client.eventHandler[event]?.(data, client, serverFromDb).catch((err) => 
         client.errorHandler.send("Uncaught event error", [errorEmbed(err, { server: serverId, event })])
     );
