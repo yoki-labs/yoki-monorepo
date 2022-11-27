c58b9b3af88567424a41ab4d79cc45ddde82085f
zaida04 <nico.03727+github@gmail.com>
2022-11-26 23:07:57 -0500

    chore: new env var

--- yoki/site/src/lib/Guilded.ts
+++ yoki/site/src/lib/Guilded.ts
@@ -1,5 +1,5 @@
 import { RestManager } from "@guildedjs/rest";
 
-if (process.env.NODE_ENV !== "development" && !process.env.GUILDED_TOKEN) throw new Error("Missing guilded token.");
+if (process.env.VERCEL_ENV !== "preview" && !process.env.GUILDED_TOKEN) throw new Error("Missing guilded token.");
 const rest = new RestManager({ "token": process.env.GUILDED_TOKEN! });
 export default rest;
--- yoki/site/src/lib/Prisma.ts
+++ yoki/site/src/lib/Prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-console.log(process.env)
-if (process.env.NODE_ENV !== "development" && !process.env.DATABASE_URL) throw new Error("Missing database URL.");
+if (process.env.VERCEL_ENV !== "preview" && !process.env.DATABASE_URL) throw new Error("Missing database URL.");
 const prisma = new PrismaClient();
 export default prisma;
