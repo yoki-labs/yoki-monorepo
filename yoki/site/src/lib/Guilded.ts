3cfaf3deaad5bd04a4fe2e6f8de7f5517a5a91f6
zaida04 <nico.03727+github@gmail.com>
2022-11-26 23:12:56 -0500

    chore: env

--- yoki/site/src/lib/Guilded.ts
+++ yoki/site/src/lib/Guilded.ts
@@ -1,5 +1,5 @@
 import { RestManager } from "@guildedjs/rest";
 
-if (process.env.VERCEL_ENV !== "preview" && !process.env.GUILDED_TOKEN) throw new Error("Missing guilded token.");
+if (process.env.VERCEL_ENV !== "preview" && process.env.NODE_ENV === "production" && !process.env.GUILDED_TOKEN) throw new Error("Missing guilded token.");
 const rest = new RestManager({ "token": process.env.GUILDED_TOKEN! });
 export default rest;
