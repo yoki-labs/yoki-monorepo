366c874c00125bd8f6d736fbe93693b984f0216c
zaida04 <nico.03727+github@gmail.com>
2022-11-26 23:02:37 -0500

    chore: test

--- yoki/site/src/lib/Prisma.ts
+++ yoki/site/src/lib/Prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
+console.log(process.env)
 if (process.env.NODE_ENV !== "development" && !process.env.DATABASE_URL) throw new Error("Missing database URL.");
 const prisma = new PrismaClient();
 export default prisma;
