882fa82cc93eb58be4386e40fcdc1dece220fe36
Nico <nico.03727+github@gmail.com>
2022-11-28 10:49:03 -0500

    chore: update welcome message

--- yoki/bot/src/events/BotServerMembershipCreated.ts
+++ yoki/bot/src/events/BotServerMembershipCreated.ts
@@ -18,6 +18,7 @@ export default async (packet: WSBotTeamMembershipCreated, ctx: Context) => {
   - [Set up modmail](https://www.guilded.gg/Yoki/groups/2dXLMBPd/channels/0a2069b9-2e7d-45da-9121-ab3b463f9af2/docs/324894) 📧
   - [Enable antiraid & captchas](https://www.guilded.gg/Yoki/groups/2dXLMBPd/channels/0a2069b9-2e7d-45da-9121-ab3b463f9af2/docs/343700) 🛑
   - [Filter invites & links](https://www.guilded.gg/Yoki/groups/2dXLMBPd/channels/0a2069b9-2e7d-45da-9121-ab3b463f9af2/docs/347910) 🔗
+  - [Scan NSFW images](https://www.guilded.gg/Yoki/groups/2dXLMBPd/channels/0a2069b9-2e7d-45da-9121-ab3b463f9af2/docs/348823)
 
   Want to explore more commands? Run the \`?help\` command!
   🔗[Support Server](https://yoki.gg/support) • 🖥️ [Website](https://yoki.gg)"
