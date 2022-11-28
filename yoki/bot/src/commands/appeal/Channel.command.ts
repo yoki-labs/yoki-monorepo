3a6979e8b8afc53a102506bb47347432eef13657
zaida04 <nico.03727+github@gmail.com>
2022-11-27 20:56:38 -0500

    chore: rename appealChannel db prop to appealChannelId

--- yoki/bot/src/commands/appeal/Channel.command.ts
+++ yoki/bot/src/commands/appeal/Channel.command.ts
@@ -18,11 +18,11 @@ const Channel: Command = {
 	execute: async (message, args, ctx, commandCtx) => {
 		const channel = args.channel as ServerChannelPayload | null;
 		if (!channel) {
-			const fetchedChannel = commandCtx.server.appealChannel ? await ctx.rest.router.getChannel(commandCtx.server.appealChannel).then(x => x.channel).catch(() => commandCtx.server.appealChannel) : null;
+			const fetchedChannel = commandCtx.server.appealChannelId ? await ctx.rest.router.getChannel(commandCtx.server.appealChannelId).then(x => x.channel).catch(() => commandCtx.server.appealChannelId) : null;
 			return ctx.messageUtil.replyWithInfo(message, "Appeal channel", typeof fetchedChannel === "string" ? `Looks like the channel was deleted or I cannot access it. The current channel set has the ID of ${inlineCode(fetchedChannel)}` : fetchedChannel ? channelName(fetchedChannel.name, fetchedChannel.serverId, fetchedChannel.groupId, fetchedChannel.id) : "not set");
 		}
 
-		await ctx.prisma.server.update({ where: { id: commandCtx.server.id }, data: { appealChannel: channel.id } });
+		await ctx.prisma.server.update({ where: { id: commandCtx.server.id }, data: { appealChannelId: channel.id } });
 		return ctx.messageUtil.replyWithSuccess(message, "Appeal channel successfully set", `You have now set the appeal channel to \`${channel.name}\`. Users will be able to appeal bans to your server through [this](https://yoki.gg/appeals/${message.serverId!}) url.`);
 	},
 };
