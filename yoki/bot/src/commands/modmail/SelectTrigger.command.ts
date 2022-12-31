13161ff92d74fbd51558f1f245565461a9e0199b
zaida04 <nico.03727+github@gmail.com>
2022-12-31 17:04:25 -0500

    fix: modmail group and category check on sendtrigger

--- yoki/bot/src/commands/modmail/SelectTrigger.command.ts
+++ yoki/bot/src/commands/modmail/SelectTrigger.command.ts
@@ -6,67 +6,67 @@ import { Category } from "../Category";
 import type { Command } from "../Command";
 
 const SelectTrigger: Command = {
-    name: "modmail-selecttrigger",
-    subName: "selecttrigger",
-    description: "Select a modmail thread trigger.",
-    usage: "<channel-id> <message-id> <emote-id>",
-    examples: ["17bce2fd-1a95-44b5-abc3-b2ff115c62fb 9fd03b10-c4a1-4c3a-814a-5f6a7b39c632 90002554"],
-    subCommand: true,
-    requiredRole: RoleType.MOD,
-    category: Category.Modmail,
-    args: [
-        {
-            name: "targetChannelId",
-            type: "channel",
-        },
-        {
-            name: "sentMessageId",
-            type: "UUID",
-        },
-        {
-            name: "emoteId",
-            type: "number",
-        },
-    ],
-    execute: async (message, args, ctx, commandCtx) => {
-        if (!commandCtx.server.modmailGroupId)
-            return ctx.messageUtil.replyWithError(
-                message,
-                `No modmail group`,
-                "This server does not have the `modmailgroup` setting set. You can set it using the config command."
-            );
-        const targetChannel = args.targetChannelId as ServerChannelPayload;
-        if (!targetChannel) return ctx.messageUtil.replyWithError(message, `Invalid ID`, `That is not a valid channel ID!`);
+	name: "modmail-selecttrigger",
+	subName: "selecttrigger",
+	description: "Select a modmail thread trigger.",
+	usage: "<channel-id> <message-id> <emote-id>",
+	examples: ["17bce2fd-1a95-44b5-abc3-b2ff115c62fb 9fd03b10-c4a1-4c3a-814a-5f6a7b39c632 90002554"],
+	subCommand: true,
+	requiredRole: RoleType.MOD,
+	category: Category.Modmail,
+	args: [
+		{
+			name: "targetChannelId",
+			type: "channel",
+		},
+		{
+			name: "sentMessageId",
+			type: "UUID",
+		},
+		{
+			name: "emoteId",
+			type: "number",
+		},
+	],
+	execute: async (message, args, ctx, commandCtx) => {
+		if (!commandCtx.server.modmailGroupId && !commandCtx.server.modmailCategoryId)
+			return ctx.messageUtil.replyWithError(
+				message,
+				`No modmail group or category set`,
+				"You can set either by using the `?modmail group` or `?modmail category` command."
+			);
+		const targetChannel = args.targetChannelId as ServerChannelPayload;
+		if (!targetChannel) return ctx.messageUtil.replyWithError(message, `Invalid ID`, `That is not a valid channel ID!`);
 
-        const sentMessageId = args.sentMessageId as string;
-        const sentMessage = await ctx.rest.router
-            .getChannelMessage(targetChannel.id, sentMessageId)
-            .then((x) => x.message)
-            .catch(() => null);
-        if (!sentMessage) return ctx.messageUtil.replyWithError(message, `Invalid message`, `That is not a valid message!`);
+		const sentMessageId = args.sentMessageId as string;
+		const sentMessage = await ctx.rest.router
+			.getChannelMessage(targetChannel.id, sentMessageId)
+			.then((x) => x.message)
+			.catch(() => null);
+		if (!sentMessage) return ctx.messageUtil.replyWithError(message, `Invalid message`, `That is not a valid message!`);
 
-        const emoteId = args.emoteId as number;
-        void ctx.amp.logEvent({
-            event_type: "MODMAIL_SELECT_TRIGGER",
-            user_id: message.createdBy,
-            event_properties: { serverId: message.serverId },
-        });
-        await ctx.prisma.reactionAction.create({
-            data: {
-                actionType: ReactionActionType.MODMAIL,
-                channelId: targetChannel.id,
-                emoteId,
-                messageId: sentMessage.id,
-                serverId: message.serverId!,
-            },
-        });
+		const emoteId = args.emoteId as number;
+		void ctx.amp.logEvent({
+			event_type: "MODMAIL_SELECT_TRIGGER",
+			user_id: message.createdBy,
+			event_properties: { serverId: message.serverId },
+		});
+		await ctx.prisma.reactionAction.create({
+			data: {
+				actionType: ReactionActionType.MODMAIL,
+				channelId: targetChannel.id,
+				emoteId,
+				messageId: sentMessage.id,
+				serverId: message.serverId!,
+			},
+		});
 
-        return ctx.messageUtil.replyWithSuccess(
-            message,
-            "Success!",
-            `Users will now be able to open a modmail thread by reacting with a reaction of the emote with an ID of ${emoteId}`
-        );
-    },
+		return ctx.messageUtil.replyWithSuccess(
+			message,
+			"Success!",
+			`Users will now be able to open a modmail thread by reacting with a reaction of the emote with an ID of ${emoteId}`
+		);
+	},
 };
 
 export default SelectTrigger;
