001b254f97284b0c87599bde99b6ee42df2d38f9
zaida04 <nico.03727+github@gmail.com>
2023-01-01 20:52:58 -0500

    ci: delete old messages cron

--- yoki/bot/scripts/delete-old-messages.ts
+++ yoki/bot/scripts/delete-old-messages.ts
@@ -2,26 +2,25 @@ import { PrismaClient } from "@prisma/client";
 import { config } from "dotenv";
 import { join } from "path";
 
-config({ path: join(__dirname, "..", "..", "..", ".env.prod") });
-console.log(process.env.DATABASE_URL);
+if (process.env.CI !== "true") config({ path: join(__dirname, "..", "..", "..", ".env.prod") });
 const prisma = new PrismaClient();
 
 void (async () => {
-    console.log("Running old message removal...");
-    const runDate = new Date();
-    const removalDates = new Date(runDate.setDate(runDate.getDate() - 14));
-    try {
-        const removalCount = await prisma.message.deleteMany({
-            where: {
-                createdAt: {
-                    lt: removalDates,
-                },
-            },
-        });
-        console.log(`Successfully removed ${removalCount.count} messages`);
-    } catch (e: any) {
-        console.log("There was an issue performing the removal.");
-        console.error(e);
-    }
-    console.log("Database cleanup completed.");
+	console.log("Running old message removal...");
+	const runDate = new Date();
+	const removalDates = new Date(runDate.setDate(runDate.getDate() - (process.env.CI ? 7 : 14)));
+	try {
+		const removalCount = await prisma.message.deleteMany({
+			where: {
+				createdAt: {
+					lt: removalDates,
+				},
+			},
+		});
+		console.log(`Successfully removed ${removalCount.count} messages`);
+	} catch (e: any) {
+		console.log("There was an issue performing the removal.");
+		console.error(e);
+	}
+	console.log("Database cleanup completed.");
 })();
