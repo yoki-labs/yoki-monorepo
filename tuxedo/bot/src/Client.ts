0c4cc12d51828f6ceb2a276f9b6956d1de4d7431
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2023-02-14 23:18:08 +0200

    fix: localize node_modules for Tuxedo's prisma to not interfere with Yoki's

--- tuxedo/bot/src/Client.ts
+++ tuxedo/bot/src/Client.ts
@@ -9,26 +9,8 @@ import type { Command, Server } from "./typings";
  * Main class that stores utils, connections to various providers, and ws
  */
 export default class Client extends AbstractClient<Client, Server, Command> {
-    // // ws manager
-    // readonly ws = new WebSocketManager({ token: process.env.GUILDED_TOKEN });
-    // // rest manager
-    // readonly rest = new RestManager({ token: process.env.GUILDED_TOKEN });
     // database connection
     readonly prisma = new PrismaClient();
-    // // cache connection
-    // readonly redis = new RedisClient(process.env.REDIS_URL ?? "cache:6379");
-
-    // // analytics
-    // readonly amp = init(process.env.AMPLITUDE_API_KEY!);
-
-    // // global collection of all timeouts within the bot so we can cancel them when the WS connection is severed
-    // readonly timeouts = new Collection<string, NodeJS.Timeout>();
-    // // global collection of all intervals within the bot so we can cancel them when the WS connection is severed
-    // readonly intervals = new Collection<string, NodeJS.Timer>();
-    // // webhook that sends to a specific channel for errors
-    // readonly errorHandler = new WebhookClient(process.env.ERROR_WEBHOOK);
-    // // global collection of all the bots commands (parent commands, sub commands, etc.)
-    // readonly commands = new Collection<string, Command>();
 
     // Util
     readonly serverUtil: ServerUtil<Client> = new ServerUtil<Client>(this);
