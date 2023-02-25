917f451154f8ad578ccc51bd9adc7cb498f70df4
zaida04 <nico.03727+github@gmail.com>
2023-02-25 18:40:28 -0500

    chore: fix formatting on errors

--- yoki/bot/src/utils/formatters.ts
+++ yoki/bot/src/utils/formatters.ts
@@ -7,13 +7,13 @@ export const inlineCode = (code: any) => `\`${code}\``;
 export const bold = (text: any) => `**${text}**`;
 
 export function inlineQuote(text: any, maxLength?: number) {
-    // Might be number, might be undefined
-    const rawStr: string = text?.toString() || " ";
+	// Might be number, might be undefined
+	const rawStr: string = text?.toString() || " ";
 
-    // Trim the number, undefined, whatever
-    const trimmed = maxLength && rawStr.length > maxLength ? `${rawStr.substring(0, maxLength - 3)}...` : rawStr;
+	// Trim the number, undefined, whatever
+	const trimmed = maxLength && rawStr.length > maxLength ? `${rawStr.substring(0, maxLength - 3)}...` : rawStr;
 
-    return `"\`${escapeInlineCodeText(trimmed)}\`"`;
+	return `"\`${escapeInlineCodeText(trimmed)}\`"`;
 }
 
 export const highlight = (text: any) => `**__${text}__**`;
@@ -22,21 +22,21 @@ export const listInlineQuote = (str: string[] | undefined) => (typeof str === "u
 export const codeBlock = (code: any, language?: string) => `\`\`\`${language ? `${language}\n` : ""}${code}\`\`\``;
 export const quoteMarkdown = (code: string, limit: number) => `\`\`\`md\n${code.length > limit ? `${code.substring(0, limit)}...` : code}\n\`\`\``;
 export function errorEmbed(err: Error | any, additionalDetails?: Record<string, string | number | null>) {
-    const embed = new Embed()
-        .setTitle(err.name)
-        .setDescription(codeBlock((err?.stack ?? err)?.slice(0, 2040) ?? "No error provided."))
-        .addField("Message", err?.message)
-        .setColor("RED");
+	const embed = new Embed()
+		.setTitle(err.name)
+		.setDescription(codeBlock((err?.stack ?? err)?.slice(0, 2040) ?? "No error provided."))
+		.addField("Message", err?.message)
+		.setColor("RED");
 
-    if (additionalDetails)
-        embed.addField("Additional Info", codeBlock(
-            Object.keys(additionalDetails as object)
-                .map((key) => `${key}: \`${additionalDetails[key]}\``)
-                .join("\n")
-                .slice(0, 1024)
-        ));
+	if (additionalDetails)
+		embed.addField("Additional Info", codeBlock(
+			Object.keys(additionalDetails as object)
+				.map((key) => `${key}: ${additionalDetails[key]}`)
+				.join("\n")
+				.slice(0, 1024)
+		));
 
-    return embed;
+	return embed;
 }
 export const channelName = (name: string, serverId: string, groupId: string, channelId: string, type?: string) =>
-    `[#${name}](https://guilded.gg/teams/${serverId}/groups/${groupId}/channels/${channelId}/${type ?? "chat"})`;
+	`[#${name}](https://guilded.gg/teams/${serverId}/groups/${groupId}/channels/${channelId}/${type ?? "chat"})`;
