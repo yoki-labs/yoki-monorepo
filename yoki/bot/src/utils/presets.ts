ea9c5e13dc61988319b4edf8bde651d8ec8ac64c
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2023-04-15 20:24:51 +0300

    fix: presets not respecting postfix, prefix and word filtering

--- yoki/bot/src/utils/presets.ts
+++ yoki/bot/src/utils/presets.ts
@@ -3,9 +3,11 @@ import { join } from "path";
 
 import type { PresetLink, PresetPattern, PresetPatternObject } from "../typings";
 
+// 0 - should have space before/after it
+// 1 - should have letters before/after it
 const wordRest = ["", "[\\W]*"];
 
-export const wordPresets = getPresets<PresetPattern[], RegExp>("content", (preset) => new RegExp(transformPreset(preset), "s"));
+export const wordPresets = getPresets<PresetPattern[], RegExp>("content", (preset) => new RegExp(transformPresetUnit(preset), "s"));
 
 export const urlPresets = getPresets<PresetLink[], PresetLink[]>("url", (preset) => preset);
 
@@ -24,6 +26,10 @@ function getPresets<P, T>(directory: string, transform: (preset: P, name: string
     return loadedPresets;
 }
 
+function transformPresetUnit(patterns: PresetPattern[]) {
+    return `(\\W+|^)(?:${transformPreset(patterns)})(\\W+|$)`;
+}
+
 // ["abc", "def"] => "abc|def"
 function transformPreset(patterns: PresetPattern[]) {
     return patterns.map(transformPresetValue).join("|");
@@ -37,7 +43,7 @@ function transformPresetValue(pattern: PresetPattern): string {
 }
 
 function transformPresetObject(pattern: PresetPatternObject): string {
-    if (pattern.type === "WORD") return `(?:^|\\W+)(?:${transformPreset(pattern._)})(?:$|\\W+)`;
+    if (pattern.type === "WORD") return `(?:${transformPreset(pattern._)})`;
 
     // Get boolean and convert it to number (0 or 1), which is now an index of wordRest
     const prefix = Number(["PREFIX", "INFIX"].includes(pattern.type));
