10418c6ea29560800bfe278995322a4449249763
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2023-07-10 19:19:01 +0300

    fix: cooldowns, income

--- tuxedo/bot/src/commands/income/income-defaults.ts
+++ tuxedo/bot/src/commands/income/income-defaults.ts
@@ -2,46 +2,49 @@ import { DefaultIncomeType } from "@prisma/client";
 
 export const defaultIncomes: Record<DefaultIncomeType, { reward: [number, number]; cooldown: number; action: string }> = {
     [DefaultIncomeType.DAILY]: {
-        // 500-5000
-        reward: [500, 4500],
+        // 1000-10000
+        reward: [1000, 9000],
         cooldown: 24 * 60 * 60 * 1000,
         action: "Claimed a daily reward",
     },
     [DefaultIncomeType.WORK]: {
-        // 500-2500
-        reward: [500, 2000],
+        // 1000-6000
+        reward: [1000, 5000],
         cooldown: 8 * 60 * 60 * 1000,
         action: "Claimed your wage",
     },
     [DefaultIncomeType.HOBBY]: {
-        // 100-400
-        reward: [100, 300],
-        cooldown: 1 * 60 * 60 * 1000,
+        // 500-3000
+        reward: [500, 2500],
+        cooldown: 40 * 60 * 1000,
         action: "Received some hobby donations",
     },
     [DefaultIncomeType.CHORE]: {
-        reward: [50, 250],
-        cooldown: 20 * 60 * 1000,
+        // 100-1000
+        reward: [100, 900],
+        cooldown: 10 * 60 * 1000,
         action: "Done house chores",
     },
     [DefaultIncomeType.COMMUNITY]: {
-        reward: [50, 750],
-        cooldown: 30 * 60 * 1000,
+        // 250-3000
+        reward: [250, 2750],
+        cooldown: 20 * 60 * 1000,
         action: "Done community/public chores",
     },
     [DefaultIncomeType.ROB]: {
-        reward: [500, 2500],
-        cooldown: 10 * 60 * 60 * 1000,
+        // 1000-3000
+        reward: [1000, 2000],
+        cooldown: 2 * 60 * 60 * 1000,
         action: "Robbed",
     },
     [DefaultIncomeType.BLACKJACK]: {
-        // 100-1'000'000, it doesn't add 100 to the max
-        reward: [100, 1000000],
+        // 10'000-1'000'000, it doesn't add 10'000 to the max
+        reward: [10000, 1000000],
         cooldown: 60 * 1000,
         action: "Played blackjack",
     },
 };
 export const defaultCreatedCooldown = 6 * 60 * 60 * 1000;
-export const defaultCreatedReceivedCurrency = [25, 175];
+export const defaultCreatedReceivedCurrency = [1000, 2000];
 
-export const bankCooldown = 12 * 60 * 60 * 1000;
+export const bankCooldown = 2 * 60 * 60 * 1000;
