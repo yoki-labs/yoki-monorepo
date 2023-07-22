c5d4323c7c0f37eea58a4cc66553003353843faa
zaida04 <nico.03727+github@gmail.com>
2023-07-22 19:59:14 -0400

    chore: prettier

--- yoki/site/src/components/dashboard/pages/Overview.tsx
+++ yoki/site/src/components/dashboard/pages/Overview.tsx
@@ -1,10 +1,10 @@
 import { faAnglesDown, faBan, faEnvelope, faExclamationTriangle, faHeart, faImage, faLink, faPrayingHands, faShieldHalved } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Alert, Box, Typography } from "@mui/joy";
 
-import DashboardModule from "../DashboardModule";
-import { Alert, Stack, Typography } from "@mui/joy";
-import { DashboardPageProps } from "./page";
 import LabsIconCard from "../../LabsIconCard";
+import DashboardModule from "../DashboardModule";
+import { DashboardPageProps } from "./page";
 
 export default function OverviewPage(props: DashboardPageProps) {
     const { serverConfig } = props;
@@ -12,14 +12,18 @@ export default function OverviewPage(props: DashboardPageProps) {
     return (
         <div className="flex flex-col gap-16">
             <Alert variant="solid" color="warning" startDecorator={<FontAwesomeIcon icon={faExclamationTriangle} />}>
-                This dashboard is in-progress and may have some bugs. If you run into any issues, report it in{" "}
-                <a href="https://www.guilded.gg/yoki" className="font-bold hover:underline">
-                    our Guilded server
-                </a>
+                <Box sx={{ alignItems: "baseline" }}>
+                    This dashboard is in-progress and may have some bugs. If you run into any issues, report it in{" "}
+                    <a href="https://www.guilded.gg/yoki" className="font-bold hover:underline">
+                        our Guilded server
+                    </a>
+                </Box>
             </Alert>
             {/* Maybe do vertical icon cards with 4 tiers in premium tab? */}
             <LabsIconCard icon={faHeart} iconAspectRatio={0.8}>
-                <Typography level="body1" fontWeight="md">Free</Typography>
+                <Typography level="body1" fontWeight="md">
+                    Free
+                </Typography>
                 <Typography level="body2">Yoki is currently in free tier.</Typography>
             </LabsIconCard>
             <section>
@@ -34,7 +38,7 @@ export default function OverviewPage(props: DashboardPageProps) {
                         activeClassName="from-purple-500 to-blue-500"
                         isActive={serverConfig.modmailEnabled}
                         onToggle={(value) => console.log("Modmail toggle", value)}
-                        />
+                    />
                     <DashboardModule
                         name="NSFW Image Scan"
                         description="Removes any potentially NSFW images from chat and media."
@@ -43,7 +47,7 @@ export default function OverviewPage(props: DashboardPageProps) {
                         isActive={serverConfig.scanNSFW}
                         requiresPremium
                         onToggle={(value) => console.log("NSFW Image Scan toggle", value)}
-                        />
+                    />
                     <DashboardModule
                         name="Anti-raid"
                         description="Customize how the bot detects and handles suspicious accounts."
@@ -51,7 +55,7 @@ export default function OverviewPage(props: DashboardPageProps) {
                         activeClassName="from-green-500 to-blue-500"
                         isActive={serverConfig.antiRaidEnabled}
                         onToggle={(value) => console.log("Anti-raid toggle", value)}
-                        />
+                    />
                     <DashboardModule
                         name="Appeals"
                         description="Allows people to apply for an unban in your server."
@@ -59,7 +63,7 @@ export default function OverviewPage(props: DashboardPageProps) {
                         activeClassName="from-violet-500 to-cyan-500"
                         isActive={serverConfig.appealsEnabled}
                         onToggle={(value) => console.log("Appeals toggle", value)}
-                        />
+                    />
                     <DashboardModule
                         name="Auto-mod"
                         description="Filters out spam and blacklisted phrases or links."
@@ -67,7 +71,7 @@ export default function OverviewPage(props: DashboardPageProps) {
                         activeClassName="from-red-500 to-pink-500"
                         isActive={serverConfig.filterEnabled}
                         onToggle={(value) => console.log("Auto-mod toggle", value)}
-                        />
+                    />
                     <DashboardModule
                         name="Invite Filter"
                         description="Filters out invites in chat."
@@ -75,7 +79,7 @@ export default function OverviewPage(props: DashboardPageProps) {
                         activeClassName="from-red-500 to-orange-500"
                         isActive={serverConfig.filterInvites}
                         onToggle={(value) => console.log("Invite Filter toggle", value)}
-                        />
+                    />
                     <DashboardModule
                         name="Anti-hoist"
                         description="Stops people from putting symbols at the start of their name to put them above everyone else."
