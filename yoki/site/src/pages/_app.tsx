558cfb9daca058797286388a3c6d3d8667642511
zaida04 <nico.03727+github@gmail.com>
2023-08-12 20:12:30 -0400

    fix: font loading

--- yoki/site/src/pages/_app.tsx
+++ yoki/site/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/styles.css";
 
 import { CssVarsProvider } from "@mui/joy";
+import { Inter } from "@next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -29,6 +30,8 @@ type AppPropsWithLayout = AppProps<{ session: Session }> & {
 
 const queryClient = new QueryClient();
 
+const inter = Inter({ subsets: ["latin"] });
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
     const layout = Component.getLayout ?? ((page) => page);
     // const gql = useContext(gqlClientContext);
@@ -53,19 +56,15 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
                 <meta name="twitter:image" content={ogFace} />
 
                 <link rel="icon" href="/favicon.ico" />
-
-                <link rel="preload" href="/fonts/inter/Inter-Bold.ttf" as="font/ttf" crossOrigin="anonymous" />
-                <link rel="preload" href="/fonts/inter/Inter-Medium.ttf" as="font/ttf" crossOrigin="anonymous" />
-                <link rel="preload" href="/fonts/inter/Inter-Regular.ttf" as="font/ttf" crossOrigin="anonymous" />
-                <link rel="preload" href="/fonts/inter/Inter-SemiBold.ttf" as="font/ttf" crossOrigin="anonymous" />
-
                 <style>html, body, #__next {`{ width: 100%; height: 100%; }`}</style>
             </Head>
             <SessionProvider session={session}>
                 <QueryClientProvider client={queryClient}>
                     {/* <gqlClientContext.Provider value={gql}> */}
                     <CssVarsProvider defaultMode="dark" theme={theme}>
-                        <Component {...pageProps} />
+                        <main className={inter.className}>
+                            <Component {...pageProps} />
+                        </main>
                     </CssVarsProvider>
                     {/* </gqlClientContext.Provider> */}
                 </QueryClientProvider>
