81055e68de9a27b5e24b98223899b68dffa35412
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2023-09-11 18:43:20 +0300

    fix: use proper placeholders for /privacy and /terms pages

--- yoki/site/src/pages/terms.tsx
+++ yoki/site/src/pages/terms.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 
 import LandingPage from "../components/landing/LandingPage";
+import { Stack } from "@mui/joy";
+import PagePlaceholder, { PagePlaceholderIcon } from "../components/PagePlaceholder";
 
 const TOS: NextPage = () => {
     return (
         <LandingPage>
-            <div className="flex items-center justify-center h-screen">
-                <h1 className="text-6xl font-bold text-white">In Construction!</h1>
-            </div>
+            <Stack direction="column" alignItems="center" sx={{ width: "100%", pt: 16 }}>
+                <PagePlaceholder icon={PagePlaceholderIcon.Wip} title="Work in progress" description="This section has not been done yet. Come back later." />
+            </Stack>
         </LandingPage>
     );
 };
