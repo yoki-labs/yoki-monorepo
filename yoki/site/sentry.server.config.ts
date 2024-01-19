7e85c06034c02565739d832a6502b8a73d4a2ab3
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-01-18 20:15:39 -0500

    feat: update dsn

--- yoki/site/sentry.server.config.ts
+++ yoki/site/sentry.server.config.ts
@@ -5,11 +5,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://50c68ae2347b6b8ddcffcf6d5a44c76b@o1365239.ingest.sentry.io/4506594200715264",
+	dsn: "https://6ffbbd565eb04813a8092e4c08b83f64@o4506595278323712.ingest.sentry.io/4506595279306752",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
 });
