d1381cf09601faaeb1cb6eb43cdd496bbfe18782
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2024-05-29 17:34:01 +0300

    fix: attempt at fixing font

--- yoki/common/src/canvasing.ts
+++ yoki/common/src/canvasing.ts
@@ -31,7 +31,7 @@ export async function generateUserJoinBanner(name: string, avatarUrl?: string | 
     // Text
     const fontSize = 32 - name.length * 0.65;
     const highlightPadding = fontSize / 5;
-    canvasCtx.font = `bold ${fontSize}px "Space Mono", "Ubuntu Mono", "Fira Sans", "Cascadia Code", "Cascadia Mono", "Inconsolata", mono, sans-serif`;
+    canvasCtx.font = `bold ${fontSize}px "Ubuntu", "Open Sans", "Space Mono", "Ubuntu Mono", "Fira Sans", "Inconsolata", sans-serif`;
 
     const prefixLength = canvasCtx.measureText(`Welcome, `);
     const nameLength = canvasCtx.measureText(`@${name}`);
