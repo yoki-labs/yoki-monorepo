065b2d2f76ec05e724a9ba3a419aef33af382423
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-06-08 19:27:18 -0400

    chore: update tuxedo with shutdown

--- tuxedo/bot/src/index.ts
+++ tuxedo/bot/src/index.ts
@@ -1,64 +1,103 @@
-import { codeBlock, errorEmbed, setClientCommands, setClientEvents } from "@yokilabs/bot";
 import { config } from "dotenv";
-import { WebhookEmbed } from "guilded.js";
+import { Client, Embed } from "guilded.js";
 import { join } from "path";
+import { stripIndents } from "common-tags";
 
-import { TuxoClient } from "./Client";
+config({ path: join(__dirname, "..", "..", ".env.prod") });
 
-// Load env variables
-config({ path: join(__dirname, "..", "..", ".env") });
-
-// Check ENV variables to ensure we have the necessary things to start the bot up
-["DEFAULT_PREFIX", "GUILDED_TOKEN", "DATABASE_URL", "MAIN_SERVER", "ERROR_WEBHOOK"].forEach((x) => {
-    if (!process.env[x]) throw new Error(`Missing env var ${x}`);
+const client = new Client({
+    "token": process.env.GUILDED_TOKEN!,
 });
 
-const client = new TuxoClient({ token: process.env.GUILDED_TOKEN! }, process.env.DEFAULT_PREFIX!);
-
-client.ws.emitter.on("error", (err) => {
-    console.log(`[WS ERR]: ${err}`);
-    void client.errorHandler.send("Error in command usage!", [new WebhookEmbed().setDescription("[WS ERR]:").addField(`Err`, codeBlock(err)).setColor("RED")]);
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user!.name}`);
 });
 
-client.ws.emitter.on("gatewayEvent", async (event, data) => {
-    const { serverId } = data.d as { serverId?: string | null };
+client.on("messageCreated", async message => {
+    if (!message.serverId) return;
+    if (message.authorId === client.user!.id) return;
 
-    if (!serverId) return;
+    const prefix = "t?";
 
-    const serverFromDb = await client.dbUtil
-        .getServer(serverId)
-        .catch((err) => void client.errorHandler.send("Error creating/fetching server for gateway event.", [errorEmbed(err as Error, { server: serverId, event })]));
+    if (!message.content.startsWith(prefix)) return;
+    return message.reply(new Embed()
+        .setTitle("Tuxedo has shutdown.")
+        .setColor("red")
+        .setDescription(stripIndents`
+            Tuxedo will not be responding to any commands. Please refer to [this post](https://www.guilded.gg/Yoki/blog/News/Shutdown-Complete) for more information.
 
-    if (!serverFromDb || serverFromDb?.blacklisted || !serverFromDb.flags.includes("EARLY_ACCESS")) return;
+            Thank you all for being a part of this incredible journey.
 
-    return client.eventHandler[event]?.(data, client, serverFromDb).catch((err) =>
-        client.errorHandler.send("Uncaught event error", [errorEmbed(err, { server: serverId, event })])
-    );
+            Sincerely,
+            Nico, IDKGoodName, ItzNxthaniel, Codeize, Dylan, & Shay.
+            **Yoki Labs**
+        `)
+    )
 });
 
-void (async (): Promise<void> => {
-    await setClientCommands(client, join(__dirname, "commands"));
-    await setClientEvents(client, join(__dirname, "events", "guilded"));
+client.login();
 
-    try {
-        // check if the main server exists and is in the database, this check is mostly to make sure our prisma migrations are applied
-        const existingMainServer = await client.prisma.server.findMany({ where: { serverId: process.env.MAIN_SERVER } });
-        if (!existingMainServer.length) await client.dbUtil.createFreshServerInDatabase(process.env.MAIN_SERVER!);
-    } catch (e) {
-        console.log(e);
-        console.log("ERROR!: You have not applied the migrations. You must run 'pnpm migrate:dev'. Exiting...");
-        return process.exit(1);
-    }
+// import { codeBlock, errorEmbed, setClientCommands, setClientEvents } from "@yokilabs/bot";
+// import { config } from "dotenv";
+// import { WebhookEmbed } from "guilded.js";
+// import { join } from "path";
 
-    try {
-        // connect redis & ws connection
-        await client.init();
+// import { TuxoClient } from "./Client";
 
-        await client.giveawayUtil.cacheGiveaways();
-        client.giveawayUtil.tick();
-        client.lifetimedUtil.tick();
-    } catch (e) {
-        console.error(e);
-        return process.exit(1);
-    }
-})();
+// // Load env variables
+// config({ path: join(__dirname, "..", "..", ".env") });
+
+// // Check ENV variables to ensure we have the necessary things to start the bot up
+// ["DEFAULT_PREFIX", "GUILDED_TOKEN", "DATABASE_URL", "MAIN_SERVER", "ERROR_WEBHOOK"].forEach((x) => {
+//     if (!process.env[x]) throw new Error(`Missing env var ${x}`);
+// });
+
+// const client = new TuxoClient({ token: process.env.GUILDED_TOKEN! }, process.env.DEFAULT_PREFIX!);
+
+// client.ws.emitter.on("error", (err) => {
+//     console.log(`[WS ERR]: ${err}`);
+//     void client.errorHandler.send("Error in command usage!", [new WebhookEmbed().setDescription("[WS ERR]:").addField(`Err`, codeBlock(err)).setColor("RED")]);
+// });
+
+// client.ws.emitter.on("gatewayEvent", async (event, data) => {
+//     const { serverId } = data.d as { serverId?: string | null };
+
+//     if (!serverId) return;
+
+//     const serverFromDb = await client.dbUtil
+//         .getServer(serverId)
+//         .catch((err) => void client.errorHandler.send("Error creating/fetching server for gateway event.", [errorEmbed(err as Error, { server: serverId, event })]));
+
+//     if (!serverFromDb || serverFromDb?.blacklisted || !serverFromDb.flags.includes("EARLY_ACCESS")) return;
+
+//     return client.eventHandler[event]?.(data, client, serverFromDb).catch((err) =>
+//         client.errorHandler.send("Uncaught event error", [errorEmbed(err, { server: serverId, event })])
+//     );
+// });
+
+// void (async (): Promise<void> => {
+//     await setClientCommands(client, join(__dirname, "commands"));
+//     await setClientEvents(client, join(__dirname, "events", "guilded"));
+
+//     try {
+//         // check if the main server exists and is in the database, this check is mostly to make sure our prisma migrations are applied
+//         const existingMainServer = await client.prisma.server.findMany({ where: { serverId: process.env.MAIN_SERVER } });
+//         if (!existingMainServer.length) await client.dbUtil.createFreshServerInDatabase(process.env.MAIN_SERVER!);
+//     } catch (e) {
+//         console.log(e);
+//         console.log("ERROR!: You have not applied the migrations. You must run 'pnpm migrate:dev'. Exiting...");
+//         return process.exit(1);
+//     }
+
+//     try {
+//         // connect redis & ws connection
+//         await client.init();
+
+//         await client.giveawayUtil.cacheGiveaways();
+//         client.giveawayUtil.tick();
+//         client.lifetimedUtil.tick();
+//     } catch (e) {
+//         console.error(e);
+//         return process.exit(1);
+//     }
+// })();
